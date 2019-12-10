6ecc79f5a9b2dea5abfc375da431888c3f47bb09
Danielius <dnlspasin@gmail.com>
2019-12-10 13:56:45 +0200

    timer

--- app/src/main/java/com/example/clockcustomview/ClockView.kt
+++ app/src/main/java/com/example/clockcustomview/ClockView.kt
@@ -1,12 +1,13 @@
 package com.example.clockcustomview
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
+import android.os.Handler
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.os.HandlerCompat
+import java.util.*
+import java.util.concurrent.Executors
 
 class ClockView : View {
     lateinit var outerCirclePaint: Paint
@@ -15,13 +16,27 @@ class ClockView : View {
     lateinit var tickPaint: Paint
     private var innerCircleSize: Float = 0f
     private var ticksRadius = 0f
+    lateinit var path: Path
+    lateinit var ticksRect: RectF
+
+    var currentTimeMs: Int = 0
+        set(value) {
+            field = value
+            invalidate()
+        }
+    var totalTimeMs = 0
+    var timeHandler = Handler()
+    val timeThread : Thread by lazy { Thread(Runnable {
+
+    }) }
     constructor(context: Context?) : super(context) {
 
     }
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    {
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
         init(attrs)
     }
+
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
@@ -54,12 +69,12 @@ class ClockView : View {
         innerCircleSize = ta.getFloat(R.styleable.ClockView_innerCircleSize, 0f)
 
         tickPaint = Paint().apply {
-            color = Color.WHITE;
-            strokeWidth = 1.5f;
-            style = Paint.Style.STROKE;
-        }
-        ticksRadius = (generalSize - innerCircleSize) / 2
+            color = Color.RED;
 
+        }
+        ticksRect = RectF()
+        ticksRadius = (generalSize - innerCircleSize) / 2
+        path = Path()
         ta.recycle()
     }
 
@@ -68,9 +83,30 @@ class ClockView : View {
         super.onDraw(canvas)
         val middleX = canvas?.width!! / 2f
         val middleY = canvas?.height!! / 2f
+        path.apply {
+
+        }
+        ticksRect.apply {
+            left = 100f
+            right = canvas?.width.toFloat() - 100
+            top = 100f
+            bottom = canvas?.height.toFloat() - 100
+        }
         canvas.drawCircle(middleX, middleY, generalSize, outerCirclePaint)
         canvas.drawCircle(middleX, middleY, innerCircleSize, innerCirclePaint)
-        canvas.drawCircle(middleX, middleY, innerCircleSize + 25, tickPaint)
+        val percent = (360 * countPercent(totalTimeMs, currentTimeMs)) / 100
+        canvas.drawArc(ticksRect, 270f, percent, true, tickPaint)
+
     }
 
+
+    fun startClock() {
+        if(timeThread.isAlive) {
+            timeThread.interrupt()
+        }
+       timeThread.start()
+    }
+
+    fun countPercent(totalTimeMs: Int, currentTimeMs: Int): Float =
+        (currentTimeMs.toFloat() / totalTimeMs) * 100f
 }
--- app/src/main/java/com/example/clockcustomview/MainActivity.kt
+++ app/src/main/java/com/example/clockcustomview/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.clockcustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var clock: ClockView
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.startButton)
+        clock = findViewById(R.id.clock)
+        clock.apply {
+            totalTimeMs = 1500
+            currentTimeMs = 25000
+        }
+        button.setOnClickListener(this)
+        Thread(Runnable {
+            Handler()
+        })
     }
+
+    override fun onClick(v: View?) {
+        clock.startClock()
+    }
+
+
+
 }
